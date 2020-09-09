ec2029c793d640030a68b2eac2cbafd130e91ec0
Sam <exodus@Samuels-MacBook-Pro-2.local>
2020-09-08 23:30:22 -0500

    initial commit

---
+++ src/test/java/com/ademolasamuel/EasyNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ademolasamuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
